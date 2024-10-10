051ba7bc7c1b94d4e2c83c499f673884b68643b2
DavidMaldo02 <dvid2016@hotmail.com>
2024-10-10 11:34:02 -0600

    separacion de backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import customerRouter from './routers/customerRouter';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/api/customers', customerRouter);
 
 const PORT = 4000;
